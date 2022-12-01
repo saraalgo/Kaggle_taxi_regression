1a152dfaf5c521d5505716629755c8083102578d
saraalgo <saraalvarezglez@gmail.com>
2022-12-01 18:22:56 +0100

    starting with feature engineering

--- 01-preprocess/src/preprocessing_cleaning.py
+++ 01-preprocess/src/preprocessing_cleaning.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 import seaborn as sns
 plt.style.use("ggplot")
-rcParams['figure.figsize'] = (15, 12)
+rcParams["figure.figsize"] = (15, 12)
 
 ## -------------------------------------------------------------------##
 ## 1. LOAD RAW DATA
@@ -37,9 +37,49 @@ if not os.path.exists("../results/raw_exploration/report_raw_data.html"):
     report_test.to_file("../results/raw_exploration/report_raw_data_test.html")
 
 ## -------------------------------------------------------------------##
-## 3. Identify output column
+## 3. Exploration of the data
+
+print("The training data has a total of {} observations and {} variables".format(train.shape[0], train.shape[1]))
+print("The testing data has a total of {} observations and {} variables".format(test.shape[0], test.shape[1]))
+
 diff_cols = set(train.columns) ^ set(test.columns)
-print("The possible output columns are: {}".format(diff_cols))
+print("The missing columns in the testing data are: {}".format(diff_cols))
+
+train.info()
+
 
 ## -------------------------------------------------------------------##
-## 4. Check data quality
+## 4. Feature engineering
+
+### id
+
+train.drop("id", axis=1, inplace=True)
+test.drop("id", axis=1, inplace=True)
+
+### vendor_id and passanger_count
+
+train.vendor_id.value_counts()
+train.passenger_count.value_counts()
+
+### pickup_datetime and dropoff_datetime
+
+train["pickup_datetime"] = pd.to_datetime(train["pickup_datetime"])
+train["hour"] = train["pickup_datetime"].dt.hour
+train["minute"] = train["pickup_datetime"].dt.minute
+train["minute_oftheday"] = train["hour"] * 60 + train["minute"]
+train["day_week"] =train["pickup_datetime"].dt.dayofweek
+train["month"] = train["pickup_datetime"].dt.month
+
+test["pickup_datetime"] = pd.to_datetime(test["pickup_datetime"])
+test["hour"] = test["pickup_datetime"].dt.hour
+test["minute"] = test["pickup_datetime"].dt.minute
+test["minute_oftheday"] = test["hour"] * 60 + test["minute"]
+test["day_week"] =test["pickup_datetime"].dt.dayofweek
+test["month"] = test["pickup_datetime"].dt.month
+
+train.drop(["pickup_datetime", "dropoff_datetime"], axis=1, inplace=True)
+test.drop(["pickup_datetime"], axis=1, inplace=True)
+
+### pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude
+
+
